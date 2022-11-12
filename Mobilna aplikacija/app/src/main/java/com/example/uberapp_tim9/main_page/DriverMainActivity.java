c686f76f7ac365dba12244721b9ba6cf55c3778b
Maja <maya.grudge.12@gmail.com>
2022-11-12 14:25:06 +0100

    Added new functionality packages and activities for member 1

---
+++ Mobilna aplikacija/app/src/main/java/com/example/uberapp_tim9/main_page/DriverMainActivity.java
@@ -0,0 +1,16 @@
+package com.example.uberapp_tim9.main_page;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.uberapp_tim9.R;
+
+public class DriverMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_main);
+    }
+}
