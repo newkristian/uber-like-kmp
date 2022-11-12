902e93b858b53ab5203fc268bf0bab95f3db6767
Maja <maya.grudge.12@gmail.com>
2022-11-12 13:55:05 +0100

    Added first mobile application files and folders

---
+++ Mobilna aplikacija/app/src/main/java/com/example/uberapp_tim9/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.uberapp_tim9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.uberapp_tim9.login.LoginActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent transition = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(transition);
+                finish();
+            }
+        }, 1000);
+    }
+
+}
