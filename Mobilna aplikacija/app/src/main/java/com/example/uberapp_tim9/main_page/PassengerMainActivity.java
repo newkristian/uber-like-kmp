7326cb6bae24ec41b12cb310f69bac0e2be8acc3
Vuk Radmilović <vuk.radmilovic01@gmail.com>
2022-11-13 12:43:35 +0100

    Added passenger UI fragments, navbar
    Added passenger UI fragments, navbar
    Deleted passenger UI activities

--- Mobilna aplikacija/app/src/main/java/com/example/uberapp_tim9/main_page/PassengerMainActivity.java
+++ Mobilna aplikacija/app/src/main/java/com/example/uberapp_tim9/main_page/PassengerMainActivity.java
@@ -1,17 +1,54 @@
 package com.example.uberapp_tim9.main_page;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import com.example.uberapp_tim9.R;
+import com.example.uberapp_tim9.databinding.ActivityPassengerMainBinding;
+import com.google.android.material.navigation.NavigationView;
 
 public class PassengerMainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityPassengerMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passenger_main);
+
+        binding = ActivityPassengerMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarPassengerMain.passengerToolbar);
+        DrawerLayout drawer = binding.passengerDrawerLayout;
+
+        NavigationView navigationView = binding.passengerNavView;
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.passenger_nav_home, R.id.passenger_nav_account, R.id.passenger_nav_inbox, R.id.passenger_nav_ride_history)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_passenger_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.passenger_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_passenger_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
     }
 
     @Override
